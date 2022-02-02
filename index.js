a0813d3193fcc50160884f5cfa56aaf6f7c4a264
ZiaulhaqHaqyar <fnu.ziaulhaq@flatironschool.com>
2022-02-02 07:57:53 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+//console.log(cats);
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+//destructivelyAppendCat();
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+//destructivelyPrependCat();
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+//destructivelyRemoveFirstCat();
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+//destructivelyRemoveFirstCat();
+
+function appendCat(name) {
+   return [...cats, "Broom"];
+}
+
+function prependCat(name) {
+    return ["Arnold", ...cats];
+}
+function removeLastCat() {
+    return cats.slice();
+}
+function removeFirstCat() {
+    return cats.slice(-2);
+}
+function removeLastCat() {
+    return cats.slice(-0, -1);
+}
